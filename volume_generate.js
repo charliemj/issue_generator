0d8f24d1a7b6ad3da4568cb227b6f4d38850c757
Karleigh Moore <karleighmoore19@gmail.com>
2017-12-02 21:56:57 -0500

    Initial

---
+++ volume_generate.js
@@ -0,0 +1,6 @@
+//given a google spreadsheet of issue dates, generates folders and appropriate docs for each issue in a given drive
+//each folder should have permissions as specified in a "Permissions" google sheet
+//each issue folder will be named N_issueNumber_ and have a spreadsheet of the same title inside
+//there will also be a schedule and inshort google doc
+//there will be fields for article title, author, byline, word count, and editing status
+//depending on editing status, emails will be sent to section editors, writers, Copy, EIC, prod
