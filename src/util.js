3910dfee17e8a188355c3b18a8f746caf7cdf242
Karleigh Moore <karleighmoore19@gmail.com>
2017-12-17 20:04:30 -0500

    Create some util functions

---
+++ src/util.js
@@ -0,0 +1,10 @@
+function makeFolder(name){
+    //https://developers.google.com/drive/v3/web/folder
+    //https://stackoverflow.com/questions/11910734/google-apps-script-how-do-i-create-a-file-in-a-folder
+}
+
+function makeSpreadSheet(name){}
+
+function makeInshort(){}
+
+function makeSportsBlitz(){}
