d5ae6650f4df2943a638f321bdd3cd81d698ca90
Karleigh Moore <karleighmoore19@gmail.com>
2017-12-25 20:27:17 -0500

    Add issue gen functions

--- src/volume_generate.js
+++ src/volume_generate.js
@@ -13,7 +13,7 @@
 //A spreadsheet in the top level directory will contain info about issue numbers, dates, and special notes (MasterSpreadsheet)
 //A script attached to this master spreadsheet will generate a folder for each issue.
     //Inside each folder there will be a master spread sheet for content for the issue
-//Inside the top level dir, there will be a folder for each Content Dept
+//Inside the top level directory, there will be a folder for each Content Dept
     //Inside each of these folders will be a folder for each issue
         //Inside each of these folders will be a spreadsheet for that department
         //The spreadsheet will have fields like author, byline, link, and editing status
@@ -27,19 +27,44 @@
 
 
 //the MasterSpreadSheet should have the following form: Issue Number, Issue Date, Special Notes
+//precondition: there exists a spreadsheet called "eic_sheet_template" that has the master sheet template
 function issueGenereration() {
+
     var ss = SpreadsheetApp.getActiveSpreadsheet(); //gets the SpreadSheet this script is attached to
     var issueSheet = ss.getSheets()[0]; //could also do ss.getSheetByName("issues");
     var departmentsSheet = ss.getSheets()[1]; //could also do ss.getSheetByName("departments");
+    var configSheet = ss.getSheers()[2]; //could also do ss.getSheetByName("config");
+
+    var folderId = configSheet.getSheetValues(1,1,1,1)[0][1];//this is magic-numbered such that the spreadsheet ID is
+    //in the 1,2 position of the sheet
+
+    //TODO: do a check to make sure this file exists and throw a useful error if not
+    //https://developers.google.com/apps-script/reference/drive/file-iterator
+    var masterIssueSpreadsheetTemplate = DriveApp.getFilesByName("eic_sheet_template").next(); //the master spreadsheet
+
+    var volumeFolder = DriveApp.getFolderById(folderId); //get folderId from config sheet
 
     //go to sheet, get all the issues and make an object for each issue with number, date, and notes
     var allIssues = getAllIssues(issueSheet);
+    var allIssueNumbers = [];
+    for (var i = 0; i < allIssues.length; i++){
+        var issue = allIssues[i];
+        var issueNum = issue.number;
+        allIssueNumbers.push(issueNum);
+        var issueDate = issue.date;
+        var issueInfo = issue.info;
+
+        //make a folder with that Issue Number and a doc with Notes
+
+        makeMasterIssueFolder(volumeFolder,issueNum);
+
+    }
+
 
     //for each issue, make a folder with the name being the number of the issue, maintain a list
     //of these issue_folders. For each folder, make an eic_spread_sheet
         //the issue object should be updated to have an id for the folder and eic_spreadsheet associated with it
     //generate each of the department folders, top level "News"
-
 }
 
 //precondition: issueSpreadsheet has 3 columns in this order: issueDate, issueNumber, issueNotes
@@ -69,9 +94,7 @@ function getAllIssues(issueSpreadsheet){
             "number":issueNumber,
             "info":issueInfo
         };
-
         allIssue.push(issue);
-
     }
     return allIssues;
 }
@@ -79,14 +102,14 @@ function getAllIssues(issueSpreadsheet){
 //TODO: need a way to detect changes to the master spreadsheet which will propagate updated
 //notes and/or additional issues and respective folders to all the dept folders + sheets
 
-//makes the top level folder a given issue
-function genGivenIssue(issueNumber){}
 
 //all of these need to be linked back to the Master Spreadsheet for the given issue
 //whenever a spreadsheet is updated in the below categories, the master spreadsheet should
 //be updated. Basically insert a row below section head?
 function genSports(){}
-function genArts(){}
+function genArts(){
+    //arts spreadsheet might be good to note any embargo
+}
 function genNews(){
     //for each issue, create a folder named the number of the issue
     //inside each folder have a dept_spreadsheet
@@ -94,12 +117,41 @@ function genNews(){
 function genScience(){}
 function genCampusLife(){}
 function genOpinion(){}
-function genPhoto(){}
+function genPhoto(){} //photospread sheet
 function genFun(){}
+function genFeatures(){}
 
 
 
+function makeMasterIssueFolder(parent,name){
+    //https://developers.google.com/drive/v3/web/folder
+    //https://stackoverflow.com/questions/11910734/google-apps-script-how-do-i-create-a-file-in-a-folder
 
+    //creates the new folder and returns it
+    folderMade = parent.createFolder(name);
+
+    //make a copy of the MasterIssueSpreadsheet and name it after current issue (name)
+    makeMasterIssueSpreadsheet(name,folderMade);
+}
+
+function makeMasterIssueSpreadsheet(name, destination){
+    //https://developers.google.com/apps-script/reference/drive/file#makeCopy(String,Folder)
+    masterIssueSpreadsheetTemplate.makeCopy(name+"_eic_master_spreadsheet",destination);
+}
+
+function makeIssueSpreadsheet(name, dept){}
+
+function makeInshort(){
+    //make a copy of the Inshort doc
+    //and add it to current folder
+    //write the editable link to the news spreadsheet
+}
+
+function makeSportsBlitz(){
+    //make a copy of the Blitz doc
+    //add it to current folder
+    //write the editable link in the sports spreadsheet
+}
 
 
 
