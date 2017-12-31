a80aa1c70c93d0ef02a25687f6eefc397c2a44dd
Karleigh Moore <karleighmoore19@gmail.com>
2017-12-31 18:14:32 -0500

    Generates inserts and sports blitz with permissions

--- src/section_specific_docs.js
+++ src/section_specific_docs.js
@@ -4,10 +4,18 @@
  * @param  {[type]} destinationFolder [description]
  * @return {[type]}                   [description]
  */
-function makeInshorts(issueNum, destinationFolder){
-    //make a copy of the Inshort doc
-    //and add it to current folder
+function makeInshorts(issueNum, destinationFolder, newsSheet){
+    //makes a new Inshorts doc for the issue and puts it in the issue folder
+    var inshorts = inshortTemplate.copy(issueNum+"_Inshorts", destinationFolder);
+    inshorts.setSharing(DriveApp.Access.ANYONE, DriveApp.Permission.EDIT);
     //write the editable link to the news spreadsheet
+    newsSheet.insertRowAfter(1);
+    //assumes the new row is at A2:H2
+    var range = newsSheet.getRange("B2:H2");
+    var editableLink = inshorts.getUrl();
+    var values = ["Inshorts","NA","NA","NA","Inshort","inshort"+volumeNumber+issueNum,editableLink,"not started"];
+   // https://developers.google.com/apps-script/reference/spreadsheet/range#setValues(Object)
+
 }
 
 /**
@@ -16,8 +24,9 @@ function makeInshorts(issueNum, destinationFolder){
  * @param  {[type]} destinationFolder [description]
  * @return {[type]}                   [description]
  */
-function makeSportsBlitz(issueNum, destinationFolder){
-    //make a copy of the Blitz doc
-    //add it to current folder
+function makeSportsBlitz(issueNum, destinationFolder, sportsSheet){
+    sportsBlitz = sportsBlitzTemplate.copy(issueNum+"_SportsBlitz", destinationFolder);
+    sportsBlitz.setSharing(DriveApp.Access.ANYONE, DriveApp.Permission.EDIT);
     //write the editable link in the sports spreadsheet
+
 }
