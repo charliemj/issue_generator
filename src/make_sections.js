f1925f2c5c58464575d7538c32be044acc8f9494
Karleigh Moore <karleighmoore19@gmail.com>
2017-12-31 18:13:55 -0500

    Added documentation

--- src/make_sections.js
+++ src/make_sections.js
@@ -26,42 +26,87 @@ function makeSectionFolders(){
                     "CampusLife":campusLife,
                     "sports":sports};
 
-
     return sectionFolders;
 }
 
 //puts an issue folder in a section folder and returns the sectionIssueFolder
 /**
  * [makeSectionIssueFolder description]
- * @param  {Folder} parent   [description]
+ * @param  {Folder} sectionFolder   [description]
  * @param  {String} dept     [description]
  * @param  {String} issueNum [description]
+ * @param {String} issueInfo [description]
  * @return {Folder}          [description]
  */
-function makeSectionIssueFolder(parent,dept,issueNum){
-    sectionIssueFolder = parent.createFolder(issueNum);
-    makeSectionSpreadsheet(issueNum, dept, sectionIssueFolder);
-    makeSectionPhotoSpreadsheet(issueNum, dept, sectionIssueFolder);
-    return sectionIssueFolder; //returns the issue folder
+function makeSectionIssueFolder(sectionFolder,dept,issueNum, issueInfo){
+    sectionIssueFolder = sectionFolder.createFolder(issueNum);
+    sectionSheet = makeSectionSpreadsheet(issueNum, dept, sectionIssueFolder);
+    issueNotes = makeIssueNotesDoc(issueInfo, issueNum, sectionIssueFolder);
+    photoSheet = makeSectionPhotoSpreadsheet(issueNum, dept, sectionIssueFolder);
+
+    var issueFolderObject = {"issueFolder":sectionIssueFolder,
+                        "sectionSheet":sectionSheet,
+                        "issueNotes":issueNotes,
+                        "photoSheet":photoSheet
+                    };
+
+    return issueFolderObject; //returns the issue folder
 }
 
-//Populates the section issue folders with spreadsheets
 /**
- * [populateSectionIssueFolders description]
- * @param  {String} issueNum       [description]
- * @param  {Dictionary} sectionFolders [description]
- * @return {[type]}                [description]
+ * Creates a section Folder for each section in the Volume directory
+ * The Folder is populated by methods within this method to include
+ * A spreadsheet and any accompanying documents/sheets
+ * @param  {String} issueNum     [description]
+ * @param  {Folder} volumeFolder [description]
+ * @param  {String} section      [description]
+ * @return {[type]}              [description]
+ */
+function makeSection(issueNum, volumeFolder, section){
+
+    sectionIssueFolderObject = makeSectionIssueFolder(volumeFolder,section,issueNum);
+
+    sectionIssueFolder = sectionIssueFolderObject.issueFolder;
+    sectionIssueSheet = sectionIssueFolderObject.sectionSheet;
+
+    if(section=="sports"){
+        makeSportsBlitz("Sports Blitz for "+issueNum, sectionIssueFolder, sectionIssueSheet);
+    }
+
+    if(section=="news"){
+        makeInshorts("Inshorts for "+issueNum, madeFolder, sectionIssueSheet);
+    }
+}
+
+
+/**
+ * Populates the section issue folders with spreadsheets
+ * @param  {String} issueNum       the issue number
+ * @param  {Dictionary} sectionFolders section names mapped to Section Folders
+ * @return  does not return anything
  */
 function populateSectionIssueFolders(issueNum, sectionFolders){
-    genSports(issueNum, sectionFolders.sports);
-    genArts(issueNum, sectionFolders.arts);
-    genNews(issueNum, sectionFolders.news);
-    genFeatures(issueNum, sectionFolders.features);
-    genOpinion(issueNum, sectionFolders.opinion);
-    genFun(issueNum, sectionFolders.fun);
-    genPhoto(issueNum, sectionFolders.photo);
-    genCampusLife(issueNum, sectionFolders.campusLife);
-    genScience(issueNum, sectionFolders.science);
+   for(var i = 0; i < sectionFolders.length; i++){
+        var section = sectionFolders[i];
+        makeSection(issueNum, parentFolder, sectionFolders[i].issueInfo, section);
+    }
+}
+
+/**
+ * [makeIssueNotesDoc description]
+ * @param  {String} issueNum          [description]
+ * @param  {Folder} destinationFolder [description]
+ * @return {[type]} [description]
+ */
+function makeIssueNotesDoc(content, issueNum, destinationFolder){
+    //takes the issue notes as input and creates a doc titled
+    //"issueNum Issue Notes" and puts it in the folder
+    var name = issueNum+" special notes";
+    issueNotesDoc = DocumentApp.create(name);
+    issueNotesDoc.appendParagraph(content);
+
+    //transform the Document into a Blob that DriveApp can work with
+    fileIssueNotesDoc = DriveApp.destinationFolder.createFile(issueNotesDoc);
 }
 
 //creates the content spreadsheet for a particular section's issue folder
@@ -73,7 +118,9 @@ function populateSectionIssueFolders(issueNum, sectionFolders){
  * @return {[type]}                   [description]
  */
 function makeSectionSpreadsheet(issueNum, dept, destinationFolder){
-    sectionSpreadsheet.copy(issueNum+"_"+dept, destinationFolder);
+    sectionSheet = sectionSpreadsheet.copy(issueNum+"_"+dept, destinationFolder);
+    sectionSheet.setSharing(DriveApp.Access.ANYONE, DriveApp.Permission.EDIT);
+    return sectionSheet;
 }
 
 //creates the photo spreadsheet for a particular section's issue folder
@@ -86,114 +133,13 @@ function makeSectionSpreadsheet(issueNum, dept, destinationFolder){
  */
 function makeSectionPhotoSpreadsheet(issueNum, dept, destinationFolder){
     sectionPhotoSpreadsheet.copy(issueNum+"_"+dept, destinationFolder);
+    sectionPhotoSpreadsheet.setSharing(DriveApp.Access.ANYONE, DriveApp.Permission.EDIT);
 }
 
 //////////////////////////////////////////////
 //////////Generates each section//////////////
 //////////////////////////////////////////////
 
-/**
- * [genSports description]
- * @param  {String} issueNum [description]
- * @param  {Folder} parent   [description]
- * @return {[type]}          [description]
- */
-function genSports(issueNum, parent){
-    var dept = "sports";
-    sectionIssueFolder = makeSectionIssueFolder(parent,dept,issueNum);
-    makeSportsBlitz("Sports Blitz for "+issueNum, sectionIssueFolder);
-    //TODO: connect Sports Blitz with the spreadsheet
-}
 
-/**
- * [genArts description]
- * @param  {String} issueNum [description]
- * @param  {Folder} parent   [description]
- * @return {[type]}          [description]
- */
-function genArts(issueNum, parent){
-    var dept = "arts";
-    makeSectionIssueFolder(parent,dept,issueNum);
-    //arts spreadsheet might be good to note any embargo
-}
-
-/**
- * [genNews description]
- * @param  {String} issueNum [description]
- * @param  {Folder} parent   [description]
- * @return {[type]}          [description]
- */
-function genNews(issueNum, parent){
-    var dept = "news";
-    madeFolder = makeSectionIssueFolder(parent,dept,issueNum);
-    makeInshorts("Inshorts for "+issueNum, madeFolder);
-    //TODO: connect inshorts with the spreadsheet
-}
-
-/**
- * [genScience description]
- * @param  {String} issueNum [description]
- * @param  {Folder} parent   [description]
- * @return {[type]}          [description]
- */
-function genScience(issueNum, parent){
-    var dept = "science";
-    makeSectionIssueFolder(parent,dept,issueNum);
-}
-
-/**
- * [genCampusLife description]
- * @param  {String} issueNum [description]
- * @param  {Folder} parent   [description]
- * @return {[type]}          [description]
- */
-function genCampusLife(issueNum, parent){
-    var dept = "campus_life";
-    makeSectionIssueFolder(parent,dept,issueNum);
-}
-
-/**
- * [genOpinion description]
- * @param  {String} issueNum [description]
- * @param  {Folder} parent   [description]
- * @return {[type]}          [description]
- */
-function genOpinion(issueNum, parent){
-    var dept = "opinion";
-    makeSectionIssueFolder(parent,dept,issueNum);
-}
-
-/**
- * [genPhoto description]
- * @param  {String} issueNum [description]
- * @param  {Folder} parent   [description]
- * @return {[type]}          [description]
- */
-function genPhoto(issueNum, parent){
-    var dept = "photo";
-    makeSectionIssueFolder(parent,dept,issueNum);
-}
-
-/**
- * [genFun description]
- * @param  {String} issueNum [description]
- * @param  {Folder} parent   [description]
- * @return {[type]}          [description]
- */
-function genFun(issueNum, parent){
-    var dept = "fun";
-    makeSectionIssueFolder(parent,dept,issueNum);
-}
-
-/**
- * [genFeatures description]
- * @param  {String} issueNum [description]
- * @param  {Folder} parent   [description]
- * @return {[type]}          [description]
- */
-function genFeatures(issueNum, parent){
-    var dept = "features";
-    makeSectionIssueFolder(parent,dept,issueNum);
-}
 
 
