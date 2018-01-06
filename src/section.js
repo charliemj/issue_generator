8832968f2ea5ef1a6eb3bff8b55f4af7cc174841
Karleigh Moore <karleighmoore19@gmail.com>
2018-01-06 14:14:58 -0500

    Bug fix

--- src/section.js
+++ src/section.js
@@ -5,8 +5,8 @@ function Section(volume, sectionName){
     this.makeAllIssuesInSection = function(){
         //makes a sectionIssueFolder for each issue
         var allIssueFolders = [];
-        for(var i in allIssueObjects){
-            var issue = allIssueObjects[i];
+        for(var i in volume.allIssueObjects){
+            var issue = volume.allIssueObjects[i];
             var sectionIssueFolder = new issueFolder(this, issue, volume);
             allIssueFolders.push(sectionIssueFolder);
         }
